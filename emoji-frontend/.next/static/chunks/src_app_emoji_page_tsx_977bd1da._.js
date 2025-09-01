43fd78b3433423220404c965afce5d0362364cba
AgytaiMukhatai <agytaimukhatai@gmail.com>
2025-09-01 18:39:03 +0500

    Remove .next build folder from Git tracking

--- emoji-frontend/.next/static/chunks/src_app_emoji_page_tsx_977bd1da._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_587b8475._.js"
-],
-    source: "dynamic"
-});
--- emoji-frontend/.next/static/chunks/src_app_page_tsx_980b3499._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_108de8ce._.js",
-  "static/chunks/src_app_home_222a1f5e.css"
-],
-    source: "dynamic"
-});
